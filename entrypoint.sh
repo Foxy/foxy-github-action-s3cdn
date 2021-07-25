c805c47e5bed0476e6d347c0939a33dc8360d657
Sami Fiaz <samifiaz@gmail.com>
2021-07-26 01:51:56 +0500

    my commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 echo "Printing text with newline"
 output=$(git status) 
-echo "PREV_TAG_HASH=$(git rev-list --tags --max-count=1)"
-echo "PREV_TAG=$(git describe --abbrev=0 --tags ${{ PREV_TAG_HASH }} )"
+echo $(git rev-list --tags --max-count=1)
 
 echo "$output"
