7fff79679de60d57964e65e1636798239f45cefe
Sami Fiaz <samifiaz@gmail.com>
2021-07-26 15:27:23 +0500

    my commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,27 @@
 
 echo "WELCOME TO MY ENTRYPOINT SH"
 
+#Fetch all tags
+git fetch --all --tags
 
 # GET Released tag
 RELEASE_TAG="${GITHUB_REF#refs/*/}"
 echo "Current release tag is: ${RELEASE_TAG}"
 
 #Get previous tag
-git fetch --all --tags > /dev/null
 PREVIOUS_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1))
 echo "Previous release tag is: ${PREVIOUS_TAG}"
 
-
 # Set dir names to be created/synced with AWS S3
+VERSION=(${RELEASE_TAG//./ })
+MAJOR=${VERSION[0]}
+MINOR=${VERSION[1]}
+PATCH=${VERSION[2]}
+
+echo "MAJOR version is: ${MAJOR}"
+echo "MINOR version is: ${MINOR}"
+echo "PATCH version is: ${PATCH}"
+
 
 #Upload to S3
 
