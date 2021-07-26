6646f467c1f5bb0e592b94d6afae2486596d2d97
Sami Fiaz <samifiaz@gmail.com>
2021-07-26 13:13:20 +0500

    my commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 echo "WELCOME TO MY ENTRYPOINT SH"
-output=$(git status) 
-output2=$(ls)
-output4=$(git rev-list --tags --max-count=1)
-output3=$(git fetch --all --tags)
-echo $(git --version)
-echo $(git branch)
-echo $(aws s3 --version)
-echo "$output4"
-echo "$output2"
+
+git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1)
+
+
+
+
+# output=$(git status) 
+# output2=$(ls)
+# output4=$(git rev-list --tags --max-count=1)
+# output3=$(git fetch --all --tags)
+# echo $(git --version)
+# echo $(git branch)
+# echo $(aws s3 --version)
+# echo "$output4"
+# echo "$output2"
