6d2bad88b6aabf5541439877f754339a8e4d2998
Sami Fiaz <samifiaz@gmail.com>
2021-07-26 13:35:43 +0500

    my commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 echo "WELCOME TO MY ENTRYPOINT SH"
+# GET Released tag
+echo "RELEASE_TAG=${GITHUB_REF#refs/*/}"
 
+#Get previous tag
 git fetch --all --tags
 git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1)
 
+# Set dir names to be created/synced with AWS S3
 
+#Upload to S3
 
 
 # output=$(git status) 
