85a3c91c77559a93ca38984e85a739e142adc63c
Sami Fiaz <samifiaz@gmail.com>
2021-07-27 12:39:36 +0500

    my commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,3 +26,27 @@ echo "major $MAJOR minor $MINOR patch $PATCH"
 
 
 #Upload to S3
+aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
+
+# Sync using our dedicated profile and suppress verbose messages.
+# All other flags are optional via the `args:` directive.
+# sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+#                 --profile s3-sync-action \
+#                 --no-progress \
+#                 ${ENDPOINT_APPEND} $*"
+
+# # Clear out credentials after we're done.
+# # We need to re-run `aws configure` with bogus input instead of
+# # deleting ~/.aws in case there are other credentials living there.
+# # https://forums.aws.amazon.com/thread.jspa?threadID=148833
+# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+# null 
+# null
+# null
+# text
+# EOF
