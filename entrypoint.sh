aa1bbf2a03f7fcbb36d220820b49da99f95e52fe
Sami Fiaz <samifiaz@gmail.com>
2021-07-27 12:24:25 +0500

    my commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 echo "WELCOME TO MY ENTRYPOINT SH"
 
-
 # GET Released tag
 RELEASE_TAG="${GITHUB_REF#refs/*/}"
 echo "Current release tag is: ${RELEASE_TAG}"
@@ -13,27 +12,15 @@ echo "Current release tag is: ${RELEASE_TAG}"
 # PREVIOUS_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1))
 # echo "Previous release tag is: ${PREVIOUS_TAG}"
 
-# Set dir names to be created/synced with AWS S3 /entrypoint.sh: line 17: syntax error: unexpected "("
-#  write and exec and test .sh script with hardcoded version numbers, locally first
-VERSION=(${RELEASE_TAG//"."/ })
-MAJOR=${VERSION[0]}
-MINOR=${VERSION[1]}
-PATCH=${VERSION[2]}
+#Set dir names to be created/synced with AWS S3 
+# version="v1.2.3-beta.1"
+IFS='.'     # space is set as delimiter
+read -ra VER <<< "$RELEASE_TAG"   # str is read into an array as tokens separated by IFS
+MAJOR=${VER[0]:1:1}
+MINOR=${VER[1]}
+PATCH=${VER[2]}
 
-echo "MAJOR version is: ${MAJOR}"
-echo "MINOR version is: ${MINOR}"
-echo "PATCH version is: ${PATCH}"
+echo "major $MAJOR minor $MINOR patch $PATCH"
 
 
 #Upload to S3
-
-
-# output=$(git status) 
-# output2=$(ls)
-# output4=$(git rev-list --tags --max-count=1)
-# output3=$(git fetch --all --tags)
-# echo $(git --version)
-# echo $(git branch)
-# echo $(aws s3 --version)
-# echo "$output4"
-# echo "$output2"
