8a24e69ccf38f53616cd9a159621d3f29243b5d1
Sami Fiaz <samifiaz@gmail.com>
2021-07-27 16:16:22 +0500

    my commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -75,13 +75,15 @@ EOF
     ${ENDPOINT_APPEND} $*"
 
 #Uploads minor version to it's respective AWS S3 dir
-if [ "$MINOR" -ne "" -o ] then
+if [ "$MINOR" -ne "" -o ] 
+then
   sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${MINOR} \
       --profile s3-sync-action \
       --no-progress \
       ${ENDPOINT_APPEND} $*"
   #Uploads PATCH version to it's respective AWS S3 dir
-  if [ "$PATCH" -ne "" ] then
+  if [ "$PATCH" -ne "" ] 
+  then
     sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${PATCH} \
         --profile s3-sync-action \
         --no-progress \
