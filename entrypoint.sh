9af1a154d6ce610d948668f98090cc1981738ecb
Sami <samifiaz@gmail.com>
2021-08-06 00:56:52 +0500

    fix: sets default value for SOURCE_DIR

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,10 @@ fi
 RELEASE_TAG="${GITHUB_REF#refs/*/}"
 echo "Current release tag is: ${RELEASE_TAG}"
 
+if [ -z "$SOURCE_DIR" ]; then
+  SOURCE_DIR='dist'
+fi
+
 if [ -z "$1" ]; then
   PACKAGE_NAME=$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}')
 else
